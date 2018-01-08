5fb3eca299e0f36758751ff3cb739d5f1ea75460
davidyunius <32894219+davidyunius@users.noreply.github.com>
2018-01-08 21:55:09 +0700

    upload ulang

--- recursive-roman.js
+++ recursive-roman.js
@@ -2,12 +2,15 @@ var roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "
 var ara = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
 function convert(num) {
-    if (num <= 0 || num >= 4000) return num;
-    var romanNumeral = "";
-    for (var i = 0; i < roman.length; i++) {
-        while (num >= ara[i]) {
-            num -= ara[i];
-            romanNumeral += roman[i];
+    if (num <= 0 || num >= 4000) {
+        return num;
+    } else {
+        var romanNumeral = "";
+        for (var i = 0; i < roman.length; i++) {
+            while (num >= ara[i]) {
+                num -= ara[i];
+                romanNumeral += roman[i];
+            }
         }
     }
     return romanNumeral;
